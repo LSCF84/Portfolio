4772c77cc70839a5ded630ae6e18ebb491375e03
Luis Sergio <62592632+LSCF84@users.noreply.github.com>
2025-11-24 19:50:31 +0100

    Update route.ts

--- src/app/api/page-data/route.ts
+++ src/app/api/page-data/route.ts
@@ -5,40 +5,24 @@ const contactBar = {
   contactItems: [
     {
       type: "email",
-      label: "hello@imsruthi.com",
+      label: "l.sergio.camacho.fernandez@gmail.com",
       icon: "/images/icon/mail-icon.svg",
-      link: "mailto:hello@imsruthi.com"
+      link: "mailto:l.sergio.camacho.fernandez@gmail.com"
     },
-    {
-      type: "phone",
-      label: "+901 5588 2500",
-      icon: "/images/icon/call-icon.svg",
-      link: "tel:+90155882500"
-    },
-    {
+     {
       type: "website",
-      label: "www.mywebsite.com",
+      label: "www.lscf84.github.io",
       icon: "/images/icon/web-icon.svg",
       link: "https://www.mywebsite.com"
     }
   ],
   socialItems: [
-    {
-      platform: "dribbble",
-      icon: "/images/icon/dribble-icon.svg",
-      link: "https://dribbble.com"
-    },
-    {
+       {
       platform: "linkedin",
       icon: "/images/icon/linkedin-icon.svg",
-      link: "https://linkedin.com"
+      link: "https://www.linkedin.com/in/luis-sergio-camacho-fernandez-a49209141/"
     },
-    {
-      platform: "facebook",
-      icon: "/images/icon/facebook-icon.svg",
-      link: "https://facebook.com"
-    }
-  ]
+    ]
 };
 
 
